43817837795e305966e9f3ec855ce2f0c28cb3ce
Czenhe <105479903+czenhee@users.noreply.github.com>
2023-05-23 16:05:10 +0800

    Activity 5
    Activity 5

---
+++ Act5DataLoading/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.PayrollDataSet = New Act5DataLoading.PayrollDataSet()
+        Me.PaySlipBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.PaySlipTableAdapter = New Act5DataLoading.PayrollDataSetTableAdapters.PaySlipTableAdapter()
+        Me.IDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NAMEDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BASICPAYDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GROSSAMOUNTDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.WATXDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PayrollDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PaySlipBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.White
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IDDataGridViewTextBoxColumn, Me.NAMEDataGridViewTextBoxColumn, Me.BASICPAYDataGridViewTextBoxColumn, Me.GROSSAMOUNTDataGridViewTextBoxColumn, Me.WATXDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.PaySlipBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.DataGridView1.Size = New System.Drawing.Size(518, 345)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(218, 374)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 35)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "PRINT"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PrintDocument1
+        '
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Document = Me.PrintDocument1
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'PayrollDataSet
+        '
+        Me.PayrollDataSet.DataSetName = "PayrollDataSet"
+        Me.PayrollDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'PaySlipBindingSource
+        '
+        Me.PaySlipBindingSource.DataMember = "PaySlip"
+        Me.PaySlipBindingSource.DataSource = Me.PayrollDataSet
+        '
+        'PaySlipTableAdapter
+        '
+        Me.PaySlipTableAdapter.ClearBeforeFill = True
+        '
+        'IDDataGridViewTextBoxColumn
+        '
+        Me.IDDataGridViewTextBoxColumn.DataPropertyName = "ID"
+        Me.IDDataGridViewTextBoxColumn.HeaderText = "ID"
+        Me.IDDataGridViewTextBoxColumn.Name = "IDDataGridViewTextBoxColumn"
+        '
+        'NAMEDataGridViewTextBoxColumn
+        '
+        Me.NAMEDataGridViewTextBoxColumn.DataPropertyName = "NAME"
+        Me.NAMEDataGridViewTextBoxColumn.HeaderText = "NAME"
+        Me.NAMEDataGridViewTextBoxColumn.Name = "NAMEDataGridViewTextBoxColumn"
+        '
+        'BASICPAYDataGridViewTextBoxColumn
+        '
+        Me.BASICPAYDataGridViewTextBoxColumn.DataPropertyName = "BASICPAY"
+        Me.BASICPAYDataGridViewTextBoxColumn.HeaderText = "BASICPAY"
+        Me.BASICPAYDataGridViewTextBoxColumn.Name = "BASICPAYDataGridViewTextBoxColumn"
+        '
+        'GROSSAMOUNTDataGridViewTextBoxColumn
+        '
+        Me.GROSSAMOUNTDataGridViewTextBoxColumn.DataPropertyName = "GROSSAMOUNT"
+        Me.GROSSAMOUNTDataGridViewTextBoxColumn.HeaderText = "GROSSAMOUNT"
+        Me.GROSSAMOUNTDataGridViewTextBoxColumn.Name = "GROSSAMOUNTDataGridViewTextBoxColumn"
+        '
+        'WATXDataGridViewTextBoxColumn
+        '
+        Me.WATXDataGridViewTextBoxColumn.DataPropertyName = "WATX"
+        Me.WATXDataGridViewTextBoxColumn.HeaderText = "WATX"
+        Me.WATXDataGridViewTextBoxColumn.Name = "WATXDataGridViewTextBoxColumn"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(542, 449)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Data Loading"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PayrollDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PaySlipBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+    Friend WithEvents PayrollDataSet As PayrollDataSet
+    Friend WithEvents PaySlipBindingSource As BindingSource
+    Friend WithEvents PaySlipTableAdapter As PayrollDataSetTableAdapters.PaySlipTableAdapter
+    Friend WithEvents IDDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents NAMEDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents BASICPAYDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents GROSSAMOUNTDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents WATXDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+End Class
---
+++ Act5DataLoading/Form1.vb
@@ -0,0 +1,21 @@
+﻿
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'PayrollDataSet.PaySlip' table. You can move, or remove it, as needed.
+        Me.PaySlipTableAdapter.Fill(Me.PayrollDataSet.PaySlip)
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PrintPreviewDialog1.ShowDialog()
+        PrintDocument1.Print()
+    End Sub
+
+    Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+        Dim bm As New Bitmap(Me.DataGridView1.Width, Me.DataGridView1.Height)
+        DataGridView1.DrawToBitmap(bm, New Rectangle(30, 30, Me.DataGridView1.Width, Me.DataGridView1.Height))
+        e.Graphics.DrawImage(bm, 0, 0)
+    End Sub
+End Class
